1585ae9ac5609647c2f4f208b57ab681230ee186
leon5458 <919633712@qq.com>
2019-03-13 17:01:17 +0800

    提交

--- app/src/main/java/com/hly/phonemodel/MainActivity.java
+++ app/src/main/java/com/hly/phonemodel/MainActivity.java
@@ -1,9 +1,14 @@
 package com.hly.phonemodel;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            EasyPermissions.requestPermissions(this, "请打开权限", 1, perms);
+        }
+
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.e("系统参数","手机名称"+PhoneModelUtil.getSystemPhoneName());
+                Log.e("系统参数","产品名称"+PhoneModelUtil.getDeviceProduct());
                 Log.e("系统参数","手机厂商"+PhoneModelUtil.getDeviceBrand());
                 Log.e("系统参数","手机手机型号"+PhoneModelUtil.getSystemModel());
                 Log.e("系统参数","手机当前系统语言"+PhoneModelUtil.getSystemLanguage());
                 Log.e("系统参数","手机系统版本号"+PhoneModelUtil.getSystemVersion());
+                Log.e("系统参数","手机AndroidId"+PhoneModelUtil.getAndroidId(getApplicationContext()));
+                Log.e("系统参数","手机IMEI"+PhoneModelUtil.getIMEI(getApplicationContext()));
             }
         });
     }
--- app/src/main/java/com/hly/phonemodel/PhoneModelUtil.java
+++ app/src/main/java/com/hly/phonemodel/PhoneModelUtil.java
@@ -3,6 +3,7 @@ package com.hly.phonemodel;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 
 import java.util.Locale;
@@ -41,6 +42,22 @@ public class PhoneModelUtil {
         return android.os.Build.VERSION.RELEASE;
     }
 
+
+    /**
+     * 获取产品名称
+     */
+
+    public static String getDeviceProduct() {
+        return android.os.Build.PRODUCT;
+    }
+
+    /**
+     * 获取手机品牌
+     */
+    public static String getSystemPhoneName() {
+        return android.os.Build.BRAND;
+    }
+
     /**
      * 获取手机型号
      *
@@ -59,17 +76,46 @@ public class PhoneModelUtil {
         return android.os.Build.BRAND;
     }
 
+
+//     需要添加权限
+    @SuppressLint("MissingPermission")
+    public static String getIMSI(Context context) {
+        TelephonyManager mTelephonyMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return mTelephonyMgr.getSubscriberId();
+    }
+
+    /**
+     * AndroidId
+     * 在设备首次启动时，系统会随机生成一个64位的数字，
+     * 并把这个数字以16进制字符串的形式保存下来。不需要权限，
+     * 平板设备通用。获取成功率也较高，缺点是设备恢复出厂设置会重置。
+     * 另外就是某些厂商的低版本系统会有bug，返回的都是相同的AndroidId。
+     */
+    @SuppressLint("HardwareIds")
+    public static String getAndroidId(Context mContext) {
+        return Settings.Secure.getString(mContext.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+    }
+
     /**
      * 获取手机IMEI(需要“android.permission.READ_PHONE_STATE”权限)
-     *
      * @return  手机IMEI
      */
     @SuppressLint("MissingPermission")
     public static String getIMEI(Context ctx) {
-        TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Activity.TELEPHONY_SERVICE);
-        if (tm != null) {
-            return tm.getDeviceId();
+//        TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+//        String deviceId = tm.getDeviceId();
+//        if (deviceId != null) {
+//            return deviceId;
+//        }
+//        return "UnKnown";
+        String deviceId ="UnKnown";
+        try {
+            TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+            deviceId = tm.getDeviceId();
+        }catch (Exception e){
+            e.getMessage();
         }
-        return null;
+        return deviceId;
     }
 }
